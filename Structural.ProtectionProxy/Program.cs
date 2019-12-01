36178706f672f10a6f7b20c3625115ad66b89e94
dodinhnguyen <nguyendo891@yahoo.com>
2019-12-01 14:40:41 +0100

    Add project files.

---
+++ Structural.ProtectionProxy/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structural.ProtectionProxy
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car being driven");
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Car car = new Car();
+        private Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("Driver too young");
+            }
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(12)); // 22
+            car.Drive();
+        }
+    }
+}
